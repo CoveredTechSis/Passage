968e0a53685babba1674c023dc1232da72cc1a3b
henrytrust1111 <henrytrust1111@gmail.com>
2024-07-31 05:06:22 +0100

    fixing the navbar

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,37 +1,38 @@
-import React from 'react'
-import './Header.css'
-import { DropCategory } from '../DropCategory/DropCategory';
+import React from "react";
+import "./Header.css";
+import { DropCategory } from "../DropCategory/DropCategory";
 import { GoPerson } from "react-icons/go";
 import { RxCaretDown } from "react-icons/rx";
-import { RxCaretUp } from "react-icons/rx";import { IoIosSearch } from "react-icons/io";
+import { RxCaretUp } from "react-icons/rx";
+import { IoIosSearch } from "react-icons/io";
 import { useState } from "react";
-import logo from "/icons/logo.png"
+import logo from "/icons/logo.png";
 
 export const Header = () => {
   const [showDropDown, setShowDropDown] = useState(false);
   return (
-    <div className='Header__container'>
-        <div className="logo">
-            <img src={logo} alt="logo" />
-        </div>
-        <div className='searchbar'>
-          <input type="search" placeholder='Find event' />
-         
-          <button>
+    <div className="Header__container">
+      <div className="logo">
+        <img src={logo} alt="logo" />
+      </div>
+      <div className="searchbar">
+        <input type="search" placeholder="Find event" />
+        <button className="search__button">
           <IoIosSearch /> search
-          </button>
-        
+        </button>
+      </div>
+      <div className="nav">
+        <div
+          className="category__header"
+          onClick={() => setShowDropDown(!showDropDown)}
+        >
+          <h2>Category{showDropDown ? <RxCaretUp /> : <RxCaretDown />} </h2>
+          {showDropDown ? <DropCategory /> : null}
         </div>
-        <div className="nav">
-        <div className="category__header" onClick={()=>setShowDropDown(!showDropDown)}>
-          <h2>Category{showDropDown?<RxCaretUp/>: <RxCaretDown/>}  </h2>
-          {showDropDown? <DropCategory />: null}
-            </div>
-           <h4>About</h4>
-            <h4>Sell a ticket</h4>
-        </div>
-        <GoPerson />
-        
+        <h4>About</h4>
+        <h4>Sell a ticket</h4>
+      </div>
+      <GoPerson />
     </div>
-  )
-}
+  );
+};
