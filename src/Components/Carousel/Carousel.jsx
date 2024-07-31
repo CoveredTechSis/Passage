71b38594868cf96aeb26dd51f73557c55d933697
CoveredTechSis <aishaolalekan@gmail.com>
2024-07-31 11:32:50 +0200

    commiting carousel icons

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
+import './Carousel.css'
 import hero1 from  '/images/hero1.png';
 import hero2 from '/images/hero2.png';
 import hero3 from '/images/hero3.png';
+import { FaGreaterThan } from "react-icons/fa6";
+import { FaLessThan } from "react-icons/fa6";
+import { GoDotFill } from "react-icons/go";
 
 export const Carousel = () => {
     const [currentIndex, setCurrentIndex]= useState(0);
@@ -21,9 +25,18 @@ export const Carousel = () => {
   return (
     <div className='Carousel__container'>
         <div>
-        <img src={images[currentIndex] } className="image" alt={`Slide ${currentIndex + 1}`}/>
-            <button onClick = {handlePrevious}>{`<`}</button>
-            <button onClick={handleNext}>{`>`}</button>
+        <img src={images[currentIndex] } className="images" alt={`Slide ${currentIndex + 1}`}/>
+           <div className='Next__btn'>
+           <div className='btn' onClick = {handlePrevious}><FaLessThan /></div>
+
+           <div className='btn' onClick = {handlePrevious}><GoDotFill /></div>
+
+            <div className='btn' onClick = {handlePrevious}><GoDotFill /></div>
+            
+             <div className='btn' onClick = {handlePrevious}><GoDotFill /></div>
+           
+           <div className='btn' onClick={handleNext}><FaGreaterThan /></div>
+           </div>
             <p>{currentIndex}</p>
         </div>
     </div>
